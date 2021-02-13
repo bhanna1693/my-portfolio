e51c56488b0e66d017a93e122a28f4a06c186c13
BrianHanna <bhanna@messerfinancial.com>
2021-02-12 22:18:35 -0500

    master - routing structure establish

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ContactComponent} from './contact.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../shared/material/material.module';
+import {RouterModule} from '@angular/router';
+
+
+@NgModule({
+  declarations: [ContactComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{path: '', component: ContactComponent}]),
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class ContactModule {
+}
