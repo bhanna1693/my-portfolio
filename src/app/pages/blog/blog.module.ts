6b5a5f1072b89cb3b88f51202df02c778d5f03c6
brianhanna <bhanna1693@gmail.com>
2021-02-27 13:44:19 -0500

    master - blog layout est

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { BlogRoutingModule } from './blog-routing.module';
-import { BlogComponent } from './blog.component';
-import { BlogHomeComponent } from './blog-home/blog-home.component';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-import { FeaturedBlogComponent } from './blog-home/featured-blog/featured-blog.component';
+import {BlogRoutingModule} from './blog-routing.module';
+import {BlogComponent} from './blog.component';
+import {BlogHomeComponent} from './blog-home/blog-home.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
 import {MaterialModule} from '../../shared/material/material.module';
-import { BlogListComponent } from './blog-home/blog-list/blog-list.component';
-import { BlogItemComponent } from './blog-home/blog-list/blog-item/blog-item.component';
+import {BlogListComponent} from './blog-home/blog-list/blog-list.component';
+import {BlogItemComponent} from './blog-home/blog-list/blog-item/blog-item.component';
 
 
 @NgModule({
-  declarations: [BlogComponent, BlogHomeComponent, BlogDetailComponent, FeaturedBlogComponent, BlogListComponent, BlogItemComponent],
+  declarations: [
+    BlogComponent,
+    BlogHomeComponent,
+    BlogDetailComponent,
+    BlogListComponent,
+    BlogItemComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule,
     MaterialModule
   ]
 })
-export class BlogModule { }
+export class BlogModule {
+}
